68467f537a4c8c2e43f6ece3ce10995e692f5288
DVadim001 <dvadim001@gmail.com>
2024-02-22 14:58:34 +0500

    repair_mag

--- dz4-2/main.py
+++ dz4-2/main.py
@@ -2,11 +2,11 @@ all_products = {'Весь склад': {}}
 korzina = []
 
 while True:
-    action = input('Выберите действие')
+    action = input('Выберите действие: ')
 
     if action.lower() == 'добавить':
         product_name = input('Введите продукт: ')
-        product_count = int(input('Количество:'))
+        product_count = int(input('Количество: '))
         all_products['Весь склад'][product_name] = product_count
         print(f'Продукт {product_name} успешно добавлен в количестве {product_count}')
 
@@ -19,6 +19,8 @@ while True:
             else:
                 korzina.append({product_to_buy: product_count})
                 all_products['Весь склад'][product_to_buy] -= product_count
+                if all_products['Весь склад'][product_to_buy] == 0:
+                    del all_products['Весь склад'][product_to_buy]
                 print('Продукт добавлен в корзину!')
         else:
             print('Такого товара нет!')
@@ -29,7 +31,21 @@ while True:
     elif action.lower() == 'удалить':
         product_to_delete = input('Введите товар для удаления: ')
         product_count_delete = int(input('Введите количество для удаления: '))
-        if product_to_delete in korzina[0].keys():
-            korzina.pop(product_to_delete)
+        found = False  # Флаг для отслеживания наличия товара в корзине
+        for item in korzina:
+            if product_to_delete in item:
+                if item[product_to_delete] >= product_count_delete:
+                    item[product_to_delete] -= product_count_delete
+                    if item[product_to_delete] == 0:
+                        korzina.remove(item)
+                    print(f'Удалено {product_count_delete} единиц товара {product_to_delete}')
+                else:
+                    print('В корзине нет такого количества товара для удаления')
+                found = True
+                break
+        if not found:
+            print('Товар для удаления не найден в корзине')
+
     else:
         print('Неизвестная операция')
+
